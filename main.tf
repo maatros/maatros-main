01c0793e05f3a739678e974b3103a56f4166fe75
maatros <kosin.piotr@gmail.com>
2022-06-14 15:31:46 +0200

    expanded index.html

--- main.tf
+++ main.tf
@@ -135,7 +135,14 @@ resource "aws_instance" "example" {
   #!/bin/bash
   sudo apt update -y
   sudo apt install apache2 -y
-  echo "<h2>WebServer</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+  echo '<html>
+  <head>
+	<title>Terraform</title>
+  </head>
+  <body bgcolor=yellow>
+	<p style="color: red; text-align: center; font-size: 70px;">Hello World from Pipiline!</p>
+  </body>
+  </html>' > /var/www/html/index.html
   sudo systemctl start apache2
   EOF
 }
