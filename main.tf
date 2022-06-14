69a34354dbb190bb78fbd43808d71f6f09471e15
maatros <kosin.piotr@gmail.com>
2022-06-14 10:35:18 +0200

    updating main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create a Security Group for an EC2 instance
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port	  = 8080
+    to_port	    = 8080
+    protocol	  = "tcp"
+    cidr_blocks	= ["0.0.0.0/0"]
+  }
+}
+
 # Create an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-09d56f8956ab235b3"
-  instance_type = "t2.micro"
+  ami                     = "ami-09d56f8956ab235b3"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.instance.id}"]
+  
+  user_data = <<-EOF
+	      #!/bin/bash
+	      echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p 8080 &
+	      EOF
+			  
+  tags {
+    Name = "terraform-example"
+  }
+}
+
+# Output variable: Public IP address
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
