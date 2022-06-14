31a94949fea27002b5f438bc78bdcb79fdcdf8f4
maatros <kosin.piotr@gmail.com>
2022-06-14 14:58:28 +0200

    shorted variables

--- variables.tf
+++ variables.tf
@@ -10,12 +10,12 @@ variable "cidr" {
 
 variable "publicSubnetCIDR" {
   type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default = ["10.0.1.0/24"]
 }
 
 variable "privateSubnetCIDR" {
   type    = list(string)
-  default = ["10.0.10.0/24", "10.0.20.0/24", "10.0.30.0/24"]
+  default = ["10.0.10.0/24"]
 }
 
 variable "environment" {
